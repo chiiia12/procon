6de20acb369d7c193f163ea6c2f628890456663b
chiiia12 <llnsbchsep12@gmail.com>
2017-12-30 19:41:32 +0900

    ex01 wip

--- src/learnalgorithm/Ex01.java
+++ src/learnalgorithm/Ex01.java
@@ -1,4 +1,16 @@
 package learnalgorithm;
 
 public class Ex01 {
+    public static void main(String[] args) {
+
+        System.out.println(powmod(2, 2, 3));
+    }
+
+    private static int powmod(int a, int k, int m) {
+        int pow = 1;
+        for (int i = 0; i < k; i++) {
+            pow = pow * a;
+        }
+        return pow % m;
+    }
 }
