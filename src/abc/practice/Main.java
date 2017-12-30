c978f6a1e415b2afda2cec11b383da64ccefb338
chiiia12 <llnsbchsep12@gmail.com>
2017-12-30 20:33:50 +0900

    abc/practice

---
+++ src/abc/practice/Main.java
@@ -0,0 +1,18 @@
+package abc.practice;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        int a, b, c;
+        String s;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        s = sc.next();
+        System.out.println(a + b + c + " " + s);
+    }
+}
