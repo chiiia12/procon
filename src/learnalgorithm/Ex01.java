7236aeeefc12769f24897930dbcfe9ef078d5ce6
chiiia12 <llnsbchsep12@gmail.com>
2017-12-31 18:31:55 +0900

    ex01 fix

--- src/learnalgorithm/Ex01.java
+++ src/learnalgorithm/Ex01.java
@@ -3,14 +3,18 @@ package learnalgorithm;
 public class Ex01 {
     public static void main(String[] args) {
 
-        System.out.println(powmod(2, 2, 3));
+        System.out.println(powmod(3, Integer.MAX_VALUE, 10000));
     }
 
     private static int powmod(int a, int k, int m) {
-        int pow = 1;
-        for (int i = 0; i < k; i++) {
-            pow = pow * a;
+        if (k == 0) {
+            return 1;
         }
-        return pow % m;
+        long pow = powmod(a, k / 2, m);
+        pow = (pow * pow) % m;
+        if (k % 2 == 1) {
+            pow = (pow * a) % m;
+        }
+        return (int) pow;
     }
 }
