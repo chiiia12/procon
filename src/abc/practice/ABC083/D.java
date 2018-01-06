e70b2e4bcb97238e57a7c4864490e24f4a64867e
chiiia12 <llnsbchsep12@gmail.com>
2018-01-06 22:38:09 +0900

    ABC083/D wip

--- src/abc/practice/ABC083/D.java
+++ src/abc/practice/ABC083/D.java
@@ -1,22 +1,36 @@
 package abc.practice.ABC083;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class D {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        long x, y;
-        x = sc.nextLong();
-        y = sc.nextLong();
-        long num = x;
-        long count = 1;
-        while (num * 2 <= y) {
-            num = num * 2;
-//            System.out.println(num);
-            count++;
-        }
-        System.out.println(count);
+        String s;
+        s = sc.next();
+//        s = "010";
+        int count = 0;
+        char before2 = 0;
+        char before = 0;
 
+        List<Integer> answerList = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (before == s.charAt(i)) {
+                count++;
+            } else if (before2 == s.charAt(i)) {
+                count++;
+            } else {
+                answerList.add(count);
+                count = 1;
+            }
+            before2 = before;
+            before = s.charAt(i);
+
+        }
+        answerList.add(count);
+        System.out.println(answerList.stream().max(Comparator.naturalOrder()).get());
     }
 }
