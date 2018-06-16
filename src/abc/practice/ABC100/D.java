572f9dc571f143bd49e8c9c163067fdf38075107
chiiia12 <llnsbchsep12@gmail.com>
2018-06-17 00:10:40 +0900

    fix ABC100/D

--- src/abc/practice/ABC100/D.java
+++ src/abc/practice/ABC100/D.java
@@ -2,8 +2,6 @@ package abc.practice.ABC100;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,9 +13,8 @@ public class D {
     public static void main(String[] args) {
         int n = sc.nextInt();
         int m = sc.nextInt();
+        long[][] ans = new long[n][8];
         List<Cake> xlist = new ArrayList<>();
-        List<Cake> ylist = new ArrayList<>();
-        List<Cake> zlist = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             Cake c = new Cake();
             c.x = sc.nextLong();
@@ -25,136 +22,52 @@ public class D {
             c.z = sc.nextLong();
             xlist.add(c);
         }
-        ylist = new ArrayList<>(xlist);
-        zlist = new ArrayList<>(xlist);
-        xlist.sort(new Comparator<Cake>() {
-            @Override
-            public int compare(Cake o1, Cake o2) {
-                return o1.x < o2.x ? 1 : -1;
-            }
-        });
-        ylist.sort(new Comparator<Cake>() {
-            @Override
-            public int compare(Cake o1, Cake o2) {
-                return o1.y < o2.y ? 1 : -1;
-            }
-        });
-        zlist.sort(new Comparator<Cake>() {
-            @Override
-            public int compare(Cake o1, Cake o2) {
-                return o1.z < o2.z ? 1 : -1;
-            }
-        });
-//        long xsum = 0;
-//        long ysum = 0;
-//        long zsum = 0;
-//        long mxsum = 0;
-//        long mysum = 0;
-//        long mzsum = 0;
-        Long[] sum = new Long[6];
-        sum[0] = 0L;
-        sum[1] = 0L;
-        sum[2] = 0L;
-        sum[3] = 0L;
-        sum[4] = 0L;
-        sum[5] = 0L;
+        for (int i = 0; i < n; i++) {
+            ans[i][0] = xlist.get(i).x + xlist.get(i).y + xlist.get(i).z;
+            ans[i][1] = -xlist.get(i).x + xlist.get(i).y + xlist.get(i).z;
+            ans[i][2] = xlist.get(i).x - xlist.get(i).y + xlist.get(i).z;
+            ans[i][3] = -xlist.get(i).x - xlist.get(i).y + xlist.get(i).z;
+            ans[i][4] = xlist.get(i).x + xlist.get(i).y - xlist.get(i).z;
+            ans[i][5] = -xlist.get(i).x + xlist.get(i).y - xlist.get(i).z;
+            ans[i][6] = xlist.get(i).x - xlist.get(i).y - xlist.get(i).z;
+            ans[i][7] = -xlist.get(i).x - xlist.get(i).y - xlist.get(i).z;
+        }
+        long[] A = new long[8];
+        Arrays.sort(ans, (a, b) -> Long.compare(a[0], b[0]));
         for (int i = 0; i < m; i++) {
-//            xsum = xsum + xlist.get(i).x;
-//            ysum = ysum + ylist.get(i).y;
-//            zsum = zsum + zlist.get(i).z;
-//            mxsum = mxsum + xlist.get(m - i).x;
-//            mysum = mysum + ylist.get(m - i).y;
-//            mzsum = mzsum + zlist.get(m - i).z;
-            sum[0] += xlist.get(i).x;
-            sum[1] += ylist.get(i).y;
-            sum[2] += zlist.get(i).z;
-            sum[3] += xlist.get(n - i - 1).x;
-            sum[4] += ylist.get(n - i - 1).y;
-            sum[5] += zlist.get(n - i - 1).z;
+            A[0] += ans[n - 1 - i][0];
         }
-        sum[0] = Math.abs(sum[0]);
-        sum[1] = Math.abs(sum[1]);
-        sum[2] = Math.abs(sum[2]);
-        sum[3] = Math.abs(sum[3]);
-        sum[4] = Math.abs(sum[4]);
-        sum[5] = Math.abs(sum[5]);
+        Arrays.sort(ans, (a, b) -> Long.compare(a[1], b[1]));
+        for (int i = 0; i < m; i++) {
+            A[1] += ans[n - 1 - i][1];
+        }
+        Arrays.sort(ans, (a, b) -> Long.compare(a[2], b[2]));
+        for (int i = 0; i < m; i++) {
+            A[2] += ans[n - 1 - i][2];
+        }
+        Arrays.sort(ans, (a, b) -> Long.compare(a[3], b[3]));
+        for (int i = 0; i < m; i++) {
+            A[3] += ans[n - 1 - i][3];
+        }
+        Arrays.sort(ans, (a, b) -> Long.compare(a[4], b[4]));
+        for (int i = 0; i < m; i++) {
+            A[4] += ans[n - 1 - i][4];
+        }
+        Arrays.sort(ans, (a, b) -> Long.compare(a[5], b[5]));
+        for (int i = 0; i < m; i++) {
+            A[5] += ans[n - 1 - i][5];
+        }
+        Arrays.sort(ans, (a, b) -> Long.compare(a[6], b[6]));
+        for (int i = 0; i < m; i++) {
+            A[6] += ans[n - 1 - i][6];
+        }
+        Arrays.sort(ans, (a, b) -> Long.compare(a[7], b[7]));
+        for (int i = 0; i < m; i++) {
+            A[7] += ans[n - 1 - i][7];
+        }
 
-
-        List<Long> list = new ArrayList<>(Arrays.asList(sum));
-        Collections.sort(list, Collections.reverseOrder());
-        if (sum[0] == list.get(0)) {
-            int countx = 0;
-            int county = 0;
-            int countz = 0;
-            for (int i = 0; i < m; i++) {
-                countx += xlist.get(i).x;
-                county += xlist.get(i).y;
-                countz += xlist.get(i).z;
-            }
-            System.out.println(Math.abs(countx) + Math.abs(county) + Math.abs(countz));
-            return;
-        }
-        if (sum[1] == list.get(0)) {
-            int countx = 0;
-            int county = 0;
-            int countz = 0;
-            for (int i = 0; i < m; i++) {
-                countx += ylist.get(i).x;
-                county += ylist.get(i).y;
-                countz += ylist.get(i).z;
-            }
-            System.out.println(Math.abs(countx) + Math.abs(county) + Math.abs(countz));
-            return;
-        }
-        if (sum[2] == list.get(0)) {
-            int countx = 0;
-            int county = 0;
-            int countz = 0;
-            for (int i = 0; i < m; i++) {
-                countx += zlist.get(i).x;
-                county += zlist.get(i).y;
-                countz += zlist.get(i).z;
-            }
-            System.out.println(Math.abs(countx) + Math.abs(county) + Math.abs(countz));
-            return;
-        }
-        if (sum[3] == list.get(0)) {
-            int countx = 0;
-            int county = 0;
-            int countz = 0;
-            for (int i = 0; i < m; i++) {
-                countx += xlist.get(n - i - 1).x;
-                county += xlist.get(n - i - 1).y;
-                countz += xlist.get(n - i - 1).z;
-            }
-            System.out.println(Math.abs(countx) + Math.abs(county) + Math.abs(countz));
-            return;
-        }
-        if (sum[4] == list.get(0)) {
-            int countx = 0;
-            int county = 0;
-            int countz = 0;
-            for (int i = 0; i < m; i++) {
-                countx += ylist.get(n - i - 1).x;
-                county += ylist.get(n - i - 1).y;
-                countz += ylist.get(n - i - 1).z;
-            }
-            System.out.println(Math.abs(countx) + Math.abs(county) + Math.abs(countz));
-            return;
-        }
-        if (sum[5] == list.get(0)) {
-            int countx = 0;
-            int county = 0;
-            int countz = 0;
-            for (int i = 0; i < m; i++) {
-                countx += zlist.get(n - i - 1).x;
-                county += zlist.get(n - i - 1).y;
-                countz += zlist.get(n - i - 1).z;
-            }
-            System.out.println(Math.abs(countx) + Math.abs(county) + Math.abs(countz));
-            return;
-
-        }
+        Arrays.sort(A);
+        System.out.println(A[7]);
     }
 
     static class Cake {
