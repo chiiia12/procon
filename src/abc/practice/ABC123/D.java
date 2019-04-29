927e14f493d615f62f723abf36b9706736b95b42
chiiia12 <llnsbchsep12@gmail.com>
2019-04-29 13:00:27 +0900

    ABC123 D challenge1(TLE)

--- src/abc/practice/ABC123/D.java
+++ src/abc/practice/ABC123/D.java
@@ -1,5 +1,8 @@
 package abc.practice.ABC123;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class D {
@@ -12,6 +15,31 @@ public class D {
         int y = sc.nextInt();
         int z = sc.nextInt();
         int k = sc.nextInt();
-        System.out.println(4);
+        long[] a = new long[x];
+        long[] b = new long[y];
+        long[] c = new long[z];
+        for (int i = 0; i < x; i++) a[i] = sc.nextLong();
+        for (int i = 0; i < y; i++) b[i] = sc.nextLong();
+        for (int i = 0; i < z; i++) c[i] = sc.nextLong();
+        List<Long> ab = new ArrayList<>();
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                ab.add(a[i] + b[j]);
+            }
+        }
+        ab.sort(Comparator.reverseOrder());
+
+        List<Long> abc = new ArrayList<>();
+
+        for (int i = 0; i < Math.min(ab.size(), k); i++) {
+            for (int j = 0; j < z; j++) {
+                abc.add(ab.get(i) + c[j]);
+            }
+        }
+        abc.sort(Comparator.reverseOrder());
+
+        for (int i = 0; i < k; i++) {
+            System.out.println(abc.get(i));
+        }
     }
 }
