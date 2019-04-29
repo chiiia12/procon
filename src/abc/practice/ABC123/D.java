4d346c34117812dffc80411a1b6f482c63cb9f63
chiiia12 <llnsbchsep12@gmail.com>
2019-04-29 13:21:47 +0900

    ABC123 D(AC)

--- src/abc/practice/ABC123/D.java
+++ src/abc/practice/ABC123/D.java
@@ -1,6 +1,8 @@
 package abc.practice.ABC123;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
@@ -15,29 +17,26 @@ public class D {
         int y = sc.nextInt();
         int z = sc.nextInt();
         int k = sc.nextInt();
-        long[] a = new long[x];
-        long[] b = new long[y];
-        long[] c = new long[z];
-        for (int i = 0; i < x; i++) a[i] = sc.nextLong();
-        for (int i = 0; i < y; i++) b[i] = sc.nextLong();
-        for (int i = 0; i < z; i++) c[i] = sc.nextLong();
-        List<Long> ab = new ArrayList<>();
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                ab.add(a[i] + b[j]);
-            }
-        }
-        ab.sort(Comparator.reverseOrder());
-
+        Long[] A = new Long[x];
+        Long[] B = new Long[y];
+        Long[] C = new Long[z];
+        for (int i = 0; i < x; i++) A[i] = sc.nextLong();
+        for (int i = 0; i < y; i++) B[i] = sc.nextLong();
+        for (int i = 0; i < z; i++) C[i] = sc.nextLong();
         List<Long> abc = new ArrayList<>();
-
-        for (int i = 0; i < Math.min(ab.size(), k); i++) {
-            for (int j = 0; j < z; j++) {
-                abc.add(ab.get(i) + c[j]);
+        Arrays.sort(A, Collections.reverseOrder());
+        Arrays.sort(B, Collections.reverseOrder());
+        Arrays.sort(C, Collections.reverseOrder());
+        for (int a = 0; a < x; a++) {
+            for (int b = 0; b < y; b++) {
+                if (a * b > k) break;
+                for (int c = 0; c < z; c++) {
+                    if (a * b * c > k) break;
+                    abc.add(A[a] + B[b] + C[c]);
+                }
             }
         }
         abc.sort(Comparator.reverseOrder());
-
         for (int i = 0; i < k; i++) {
             System.out.println(abc.get(i));
         }
