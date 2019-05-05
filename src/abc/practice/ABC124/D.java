3598d94a174185a07fa1719221f521703746f874
chiiia12 <llnsbchsep12@gmail.com>
2019-05-05 22:59:58 +0900

    ABC124 D AC

--- src/abc/practice/ABC124/D.java
+++ src/abc/practice/ABC124/D.java
@@ -14,7 +14,7 @@ public class D {
         int k = sc.nextInt();
         String s = sc.next();
         List<Integer> list = new ArrayList<>();
-        if (s.charAt(0) == '1') {
+        if (s.charAt(0) == '0') {
             list.add(0);
         }
         int cnt = 0;
@@ -27,10 +27,13 @@ public class D {
             }
         }
         list.add(cnt);
+        if (list.size() % 2 == 0) {
+            list.add(0);
+        }
         int ans = 0;
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i += 2) {
             int left = i;
-            int right = Math.min(i + k * 2, list.size());
+            int right = Math.min(i + k * 2 + 1, list.size());
             int num = 0;
             for (int j = left; j < right; j++) {
                 num += list.get(j);
