44856266fe6769ec4812db3a1ae8c8aa4ffc4332
chiiia12 <llnsbchsep12@gmail.com>
2019-05-26 23:52:30 +0900

    fix ABC127 C

--- src/abc/practice/ABC127/C.java
+++ src/abc/practice/ABC127/C.java
@@ -12,16 +12,15 @@ public class C {
         int m = sc.nextInt();
         int[] L = new int[m];
         int[] R = new int[m];
+        int maxL = 0;
+        int minR = Integer.MAX_VALUE;
         for (int i = 0; i < m; i++) {
             L[i] = sc.nextInt();
             R[i] = sc.nextInt();
+            maxL = Math.max(maxL, L[i]);
+            minR = Math.min(minR, R[i]);
         }
-        if (m == 1) {
-            System.out.println(n);
-        } else {
-            System.out.println(R[0] == L[m - 1] ? 1 : 2);
-        }
-
+        System.out.println(Math.max(minR - maxL + 1, 0));
 
     }
 }
