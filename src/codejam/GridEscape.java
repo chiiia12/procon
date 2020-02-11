3fde14babce401072b67eff5e162ce5baa7b82ba
chiiia12 <llnsbchsep12@gmail.com>
2020-02-11 22:27:24 +0900

    tweak a bit

--- src/codejam/GridEscape.java
+++ src/codejam/GridEscape.java
@@ -19,6 +19,7 @@ public class GridEscape {
             r[i] = sc.nextInt();
             c[i] = sc.nextInt();
             k[i] = sc.nextInt();
+
             calculate(i);
         }
     }
@@ -26,28 +27,30 @@ public class GridEscape {
     private static void calculate(int index) {
         int count = 0;
         char[][] ans = new char[r[index]][c[index]];
+        boolean[][] mem = new boolean[r[index]][c[index]];
         for (int i = 0; i < r[index]; i++) {
             for (int j = 0; j < c[index]; j++) {
                 count++;
                 if (count <= k[index]) {
-                    ans[i][j] = 'N';
+                    ans[i][j] = 'W';
+                    mem[i][j] = true;
                 } else {
-                    if (i - 1 > 0 && ans[i - 1][j] != 'N') {
-                        ans[i][j] = 'N';
-                    } else if (i + 1 < r[index] && ans[i + 1][j] != 'N') {
-                        ans[i][j] = 'S';
-                    } else if (j - 1 > 0 && ans[i][j - 1] != 'N') {
+                    if (j - 1 >= 0 && !mem[i][j - 1]) {
                         ans[i][j] = 'W';
-                    } else if (j + 1 < c[index] && ans[i][j + 1] != 'N') {
+                    } else if (j + 1 < c[index] && !mem[i][j + 1]) {
                         ans[i][j] = 'E';
+                    } else if (i - 1 >= 0 && !mem[i - 1][j]) {
+                        ans[i][j] = 'N';
+                    } else if (i + 1 < r[index] && !mem[i + 1][j]) {
+                        ans[i][j] = 'S';
                     } else {
-                        System.out.println("Case #" + index + ": IMPOSSIBLE");
+                        System.out.println("Case #" + (index + 1) + ": IMPOSSIBLE");
                         return;
                     }
                 }
             }
         }
-        System.out.println("Case #" + index + ": POSSIBLE");
+        System.out.println("Case #" + (index + 1) + ": POSSIBLE");
         for (int i = 0; i < r[index]; i++) {
             for (int j = 0; j < c[index]; j++) {
                 System.out.print(ans[i][j]);
