68ba7e8a0ed4efad1b178a36d075c8bb7c2d3b73
chiiia12 <llnsbchsep12@gmail.com>
2020-02-15 14:40:13 +0900

    refactor

--- src/codejam/BurgerOptimization.java
+++ src/codejam/BurgerOptimization.java
@@ -28,16 +28,11 @@ public class BurgerOptimization {
     private static void calculate(int index) {
         int sum = 0;
         d.sort(Comparator.naturalOrder());
-        if (k % 2 == 0) {
-            for (int i = 0; i < k / 2; i++) {
-                sum += Math.pow(d.get(2 * i) - Math.abs(i), 2);
-                sum += Math.pow(d.get(2 * i + 1) - Math.abs(i), 2);
-            }
-        } else {
-            for (int i = 0; i < k / 2; i++) {
-                sum += Math.pow(d.get(2 * i) - Math.abs(i), 2);
-                sum += Math.pow(d.get(2 * i + 1) - Math.abs(i), 2);
-            }
+        for (int i = 0; i < k / 2; i++) {
+            sum += Math.pow(d.get(2 * i) - Math.abs(i), 2);
+            sum += Math.pow(d.get(2 * i + 1) - Math.abs(i), 2);
+        }
+        if (k % 2 == 1) {
             sum += Math.pow(d.get(k - 1) - Math.abs((k - 1) / 2), 2);
         }
         System.out.println("Case #" + (index + 1) + ": " + sum);
