8bcc6432e8a8fc984dbed16669c333701727e15d
chiiia12 <chiakiyokoo@gmail.com>
2020-07-09 15:23:16 +0900

    ABC172-C binarysearch(partly WA)

--- src/abc/practice/ABC172/C.java
+++ src/abc/practice/ABC172/C.java
@@ -11,35 +11,38 @@ public class C {
         int n = sc.nextInt();
         int m = sc.nextInt();
         long k = sc.nextLong();
-        long[] a = new long[n];
-        long[] b = new long[m];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextLong();
-        }
-        for (int i = 0; i < m; i++) {
-            b[i] = sc.nextLong();
-        }
-        long[][] dp = new long[n + 1][m + 1];
+        long[] a = new long[n + 1];
+        long[] b = new long[m + 1];
         int max = 0;
-        for (int i = 0; i <= n; i++) {
-            inside:
-            for (int j = 0; j <= m; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = k;
-                    continue;
-                }
-                if (j == 0 && i > 0) {
-                    dp[i][j] = dp[i - 1][j] - a[i - 1];
-                } else {
-                    dp[i][j] = dp[i][j - 1] - b[j - 1];
-                }
-                if (dp[i][j] >= 0) {
-                    max = Math.max(max, i + j);
-                } else {
-                    break inside;
-                }
+        for (int i = 1; i < n + 1; i++) {
+            a[i] = a[i - 1] + sc.nextLong();
+        }
+        for (int i = 1; i < m + 1; i++) {
+            b[i] = b[i - 1] + sc.nextLong();
+        }
+        for (int i = 0; i < n + 1; i++) {
+            long diff = k - a[i];
+            if (diff > 0) {
+                int ans = binarySearch(0, b.length - 1, diff, b);
+                max = Math.max(i + ans, max);
             }
         }
         System.out.println(max);
+    }
+
+    private static int binarySearch(int left, int right, long target, long[] arr) {
+        if (right - left <= 1) {
+            if (arr[right] <= target) {
+                return right;
+            } else {
+                return left;
+            }
+        }
+        int mid = left + ((right - left) / 2);
+        if (arr[mid] > target) {
+            return binarySearch(left, mid - 1, target, arr);
+        } else {
+            return binarySearch(mid, right, target, arr);
+        }
     }
 }
